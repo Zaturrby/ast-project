ad7122ed0820b9bc89aaae7bb6afc6ba3c902ed9
Zaturrby <rjkorteschiel@gmail.com>
2015-04-08 08:59:05 +0200

    cfk's work

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
-class Test extends React.Component {
-  render(){
-    return (
-      <h1>HELLO WORLD</h1>
-    )
-  }
-}
+var divStyle = {color: "rgb(" + "255" + "," + "123" + "," + "123" +")"};
 
-React.render(<Test />, document.body);
+var App = React.createClass({
+	update: function(e){
+		this.setState({
+			red: this.refs.red.getDOMNode().value,
+			green: this.refs.green.getDOMNode().value,
+			blue: this.refs.blue.getDOMNode().value
+		});
+	},
+	getInitialState: function(){
+		return {
+			red: 0,
+			green: 0,
+			blue: 0 
+		}
+	},
+	render: function(){
+		return (
+			<div>
+				<Widget ref="red" update={this.update} />
+				<p>{this.state.red}</p>
+				<Widget ref="green" update={this.update} />
+				<p>{this.state.green}</p>
+				<Widget ref="blue" update={this.update} />
+				<p>{this.state.blue}</p>
+				<div style={divStyle}>hello</div>
+			</div>
+		)
+	}
+});
+
+var Widget = React.createClass({
+	render: function(){
+		return (
+			<input type="range" min="0" max="255" onChange={this.props.update} />
+		)	
+	}
+});
+
+React.render(<App/>, document.body);
